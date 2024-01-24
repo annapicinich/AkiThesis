c0835c386c60a06e197f91e39bc7e5e8c49081c2
annapicinich1 <annapicinich1@gmail.com>
2024-01-24 15:27:53 -0600

    added story and button

--- my-app/src/navbar.js
+++ my-app/src/navbar.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
+import { Menu } from 'antd';
 
-import { Menu, Icon } from 'antd';
-const MenuItemGroup = Menu.ItemGroup;
+const MenuItem = Menu.Item;
 
+const menuStyles = {
+    
+      color: 'black',
+  };
+  const centerStyle = {
+    position: 'relative',
+    display: 'flex',
+    justifyContent:'center',
+  };
 
 class Navbar extends Component {
-  
-render() {
+  render() {
     return (
-        <nav className="menuBar" style={{ minWidth: 0, flex: "auto", }} >
-          
-          <Menu mode="horizontal">
-       <Menu.Item key="Stories">
-          <a href="Stories">Stories</a>
-        </Menu.Item>
-        <Menu.Item key="Supplemental Research">
-          <a href="Research">Supplemental Research</a>
-        </Menu.Item>
-        <Menu.Item key="Interactive Module">
-          <a href="Interactive">Interactive Module</a>
-        </Menu.Item>
-        <Menu.Item key="About">
-          <a href="About">About</a>
-        </Menu.Item>
-       
-        
-      </Menu>
-        </nav>
+ 
+        <Menu mode="horizontal"style={centerStyle} >
+          <MenuItem key="Stories">
+            <a href="Stories" style={menuStyles.selectedMenuItem}>Stories</a>
+          </MenuItem>
+          <MenuItem key="Supplemental Research">
+            <a href="Research" style={menuStyles.selectedMenuItem}>Supplemental Research</a>
+          </MenuItem>
+          <MenuItem key="Interactive Module">
+            <a href="Interactive" style={menuStyles.selectedMenuItem}>Interactive Module</a>
+          </MenuItem>
+          <MenuItem key="About">
+            <a href="About" style={menuStyles.selectedMenuItem}>About</a>
+          </MenuItem>
+        </Menu>
+      
     );
   }
 }
+
 export default Navbar;
--- my-app/src/screens/stories.js
+++ my-app/src/screens/stories.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card, Button } from 'antd';
 
-const Stories = () => {
-  return (
-    <div>
-      <header>
-        <h1>Stories</h1>
-      </header>
-      <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-  
-      </main>
-    </div>
-  );
+const { Meta } = Card;
+
+// Component for the story content
+class StoryContent extends Component {
+  render() {
+    return (
+        
+      <div style={{ margin: '10px', maxWidth: '600px', textAlign: 'center', overflowY: 'auto' }}>
+       
+        <p>
+          Chung’s father was born in Ohio, one of five siblings. When he was thirteen,
+          his mother (Chung’s grandmother) was diagnosed with kidney failure and
+          would soon be reliant on a home dialysis machine. This change called on all
+          five children to take on greater roles of responsibility in the family. Not sure
+          that college was a good fit for him, Chung’s father took an entry-level job at
+          American Greetings. At the same time, he began taking a few night classes
+          at the local community college.
+        </p>
+        <p>
+          Chung’s mother was also born in Ohio and was similarly one of five
+          children with significant responsibility at home. Chung’s mother hoped to
+          become a nurse and enrolled at the same local community college. Chung’s
+          mother and father met there and were quickly married. Chung’s mother
+          applied to nursing school but was then offered a job in Alaska. There was no
+          question that she would need to take this job.
+        </p>
+        <p>
+          After working in Alaska for two years, Chung’s parents relocated to Seattle
+          when her mother found work as a respiratory therapist. Here, Chung’s father
+          began a restaurant-management certificate program. He was then offered a
+          position to manage a pizza restaurant in Oregon, and they moved south.
+          They were able to find a small home in town.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default Stories;
+// Main Story Page Component
+class StoryPage extends Component {
+  render() {
+    return (
+        <div>
+            
+            <div style={{ marginLeft: '10px' }}>
+          <Button type="primary" style={{ backgroundColor: 'red' }}>Annotation Off</Button>
+        </div>
+        <h2 style = {{justifyContent: "center",display: 'flex', }}>Story 1</h2>
+      <div style={{ display: 'flex', height: '50vh', justifyContent: "center" }}>
+        
+          <StoryContent />
+        
+        
+      </div>
+      </div>
+    );
+  }
+}
+
+export default StoryPage;
